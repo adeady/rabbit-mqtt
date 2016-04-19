018feeefa94b483a12ed64a8e520f83082807273
adeady <deady.angela@gmail.com>
2016-04-19 09:13:14 -0400

    Sender publishes mqtt message

--- src/main/groovy/com/example/Sender.groovy
+++ src/main/groovy/com/example/Sender.groovy
@@ -1,22 +1,45 @@
 import com.rabbitmq.client.Channel
 import com.rabbitmq.client.Connection
 import com.rabbitmq.client.ConnectionFactory
+import org.eclipse.paho.client.mqttv3.MqttClient
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions
+import org.eclipse.paho.client.mqttv3.MqttException
+import org.eclipse.paho.client.mqttv3.MqttMessage
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence
 
 public class Sender {
     private final static String QUEUE_NAME = "hello";
 
     public static void main(String[] argv) throws java.io.IOException {
-        ConnectionFactory factory = new ConnectionFactory()
-        factory.setHost("localhost")
-        Connection connection = factory.newConnection()
-        Channel channel = connection.createChannel()
+        String topic        = "amp.topic";
+        String content      = "Message from MqttPublishSample";
+        int qos             = 2;  //setting to 0 fixed error
+        String broker       = "tcp://localhost:1883";
+        String clientId     = "JavaSample"+Math.random();
+        MemoryPersistence persistence = new MemoryPersistence();
 
-        channel.queueDeclare(QUEUE_NAME, false, false, false, null)
-        String message = "Hello World!"
-        channel.basicPublish("", QUEUE_NAME, null, message.getBytes())
-        println(" [x] Sent '" + message + "'");
-
-        channel.close()
-        connection.close()
+        try {
+            MqttClient sampleClient = new MqttClient(broker, clientId, persistence);
+            MqttConnectOptions connOpts = new MqttConnectOptions();
+            connOpts.setCleanSession(true);
+            System.out.println("Connecting to broker: "+broker);
+            sampleClient.connect(connOpts);
+            System.out.println("Connected");
+            System.out.println("Publishing message: "+content);
+            MqttMessage message = new MqttMessage(content.getBytes());
+            message.setQos(qos);
+            sampleClient.publish(topic, message);
+            System.out.println("Message published");
+            sampleClient.disconnect();
+            System.out.println("Disconnected");
+            System.exit(0);
+        } catch(MqttException me) {
+            System.out.println("reason "+me.getReasonCode());
+            System.out.println("msg "+me.getMessage());
+            System.out.println("loc "+me.getLocalizedMessage());
+            System.out.println("cause "+me.getCause());
+            System.out.println("excep "+me);
+            me.printStackTrace();
+        }
     }
 }
